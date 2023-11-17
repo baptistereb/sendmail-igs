f897d25cce89f37bb02410d21621da9f18067abf
Baptiste <baptiste.rebillard@outlook.fr>
2023-11-17 11:53:53 +0100

    fix: réarrangement des variables

--- fromaccount_to_radius.py
+++ fromaccount_to_radius.py
@@ -1,26 +1,15 @@
 import csv
 
-# Nom du fichier CSV d'entrée
 fichier_csv_entree = "account.csv"
-
-# Nom du fichier de sortie
 fichier_sortie = "radius_account"
 
-# Ouvrir le fichier CSV en lecture
 with open(fichier_csv_entree, 'r') as fichier_csv:
     lecteur_csv = csv.reader(fichier_csv)
-    
-    # Ouvrir le fichier de sortie en écriture
     with open(fichier_sortie, 'w') as fichier_sortie:
-        # Parcourir chaque ligne du fichier CSV
         for ligne in lecteur_csv:
-            # Vérifier s'il y a suffisamment d'éléments dans la ligne
             if len(ligne) == 5:
-                # Extraire le nom d'utilisateur et le mot de passe
                 nom_utilisateur = ligne[3]
                 mot_de_passe = ligne[4]
-                
-                # Écrire dans le fichier de sortie
                 fichier_sortie.write(f'"{nom_utilisateur}" Cleartext-Password := "{mot_de_passe}"\n')
 
 print("Conversion terminée.")
--- fromaccount_to_sendmail.py
+++ fromaccount_to_sendmail.py
@@ -5,15 +5,16 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+# on rentre les informations de l'adresse email emettrice (qui va également recevoir les mails en BCC pour vérifier qu'ils arrivent bien)
+email_address = 'rebillar@insa-toulouse.fr'
+
+
 smtp_address = 'smtp.insa-toulouse.fr'
 smtp_port = 465
-
+fichier_input = 'account.csv' # (à générer avec create_account.py)
 passwd = open(".passwd", "r")
 email_password = passwd.readlines()[0]
 passwd.close()
-
-# on rentre les informations de l'adresse email emettrice (qui va également recevoir les mails en BCC pour vérifier qu'ils arrivent bien)
-email_address = 'rebillar@insa-toulouse.fr'
 
 def sendmail(receiver, content):
   message = MIMEMultipart()
@@ -36,12 +37,6 @@ def content(nom_fichier, user, passwd):
   content = content.replace("id_token", user)
   content = content.replace("mdp_token", passwd)
   return content
-
-##############################
-#print(sendmail("dacostab@insa-toulouse.fr", content("email.html", "un identifiant", "un mdp")))
-
-
-fichier_input = 'account.csv' # (à générer avec create_account.py)
 
 with open(fichier_input, newline='', encoding='utf-8') as fichier_csv:
   lecteur_csv = csv.reader(fichier_csv)
