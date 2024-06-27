858eca3565506fe59d32ba90ea537b5f539cf322
Baptiste <baptiste.rebillard@outlook.fr>
2024-06-27 19:13:56 +0200

    fix: nom et prenom avec accent, espaces ou caractères spéciaux

--- create_account.py
+++ create_account.py
@@ -3,6 +3,7 @@
 import csv
 import random
 import string
+import re
 from unidecode import unidecode
 
 nom_fichier = 'list.csv'
@@ -11,13 +12,16 @@ User = []
 with open(nom_fichier, newline='', encoding='utf-8') as fichier_csv:
     lecteur_csv = csv.reader(fichier_csv)
     for ligne in lecteur_csv:
-        prenom_nom_propre = f"{unidecode(ligne[1]).lower()}.{unidecode(ligne[0]).lower()}"
+        prenom = re.sub(r'[^a-z]', '', unidecode(ligne[1]).lower())
+        nom = re.sub(r'[^a-z]', '', unidecode(ligne[0]).lower())
+        prenom_nom_propre = f"{prenom}.{nom}"
         ligne.append(prenom_nom_propre)
-        mot_de_passe = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+        rchoice = random.choices(string.ascii_letters + string.digits, k=8)
+        mot_de_passe = ''.join(rchoice)
         ligne.append(mot_de_passe)
         User.append(ligne)
 
-nouveau_nom_fichier = "output/account.csv"
-with open(nouveau_nom_fichier, 'w', newline='', encoding='utf-8') as fichier_csv:
+new = "output/account.csv"
+with open(new, 'w', newline='', encoding='utf-8') as fichier_csv:
     ecrivain_csv = csv.writer(fichier_csv)
     ecrivain_csv.writerows(User)
