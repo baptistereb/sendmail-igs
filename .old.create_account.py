60d646c9f1df1a3101e294a9c7e37f5831d3cbd1
Baptiste <baptiste.rebillard@outlook.fr>
2025-11-18 22:42:52 +0100

    chore: from website import

---
+++ .old.create_account.py
@@ -0,0 +1,32 @@
+#sudo apt install python3-unidecode
+
+# Old script before the website export
+
+
+
+"""
+import csv
+import random
+import string
+import re
+from unidecode import unidecode
+
+nom_fichier = 'list.csv'
+
+User = []
+with open(nom_fichier, newline='', encoding='utf-8') as fichier_csv:
+    lecteur_csv = csv.reader(fichier_csv)
+    for ligne in lecteur_csv:
+        prenom = re.sub(r'[^a-z]', '', unidecode(ligne[1]).lower())
+        nom = re.sub(r'[^a-z]', '', unidecode(ligne[0]).lower())
+        prenom_nom_propre = f"{prenom}.{nom}"
+        ligne.append(prenom_nom_propre)
+        rchoice = random.choices("abcdefghjkmnopqrstuvwxyzABCDEFGHJKMNOPQRSTUVWXYZ123456789", k=8)
+        mot_de_passe = ''.join(rchoice)
+        ligne.append(mot_de_passe)
+        User.append(ligne)
+
+new = "output/account.csv"
+with open(new, 'w', newline='', encoding='utf-8') as fichier_csv:
+    ecrivain_csv = csv.writer(fichier_csv)
+    ecrivain_csv.writerows(User)
--- create_account_from_site.py
+++ create_account_from_site.py
@@ -12,14 +12,18 @@ User = []
 with open(nom_fichier, newline='', encoding='utf-8') as fichier_csv:
     lecteur_csv = csv.reader(fichier_csv)
     for ligne in lecteur_csv:
+        new_line=[]
         prenom = re.sub(r'[^a-z]', '', unidecode(ligne[1]).lower())
         nom = re.sub(r'[^a-z]', '', unidecode(ligne[0]).lower())
         prenom_nom_propre = f"{prenom}.{nom}"
-        ligne.append(prenom_nom_propre)
         rchoice = random.choices("abcdefghjkmnopqrstuvwxyzABCDEFGHJKMNOPQRSTUVWXYZ123456789", k=8)
         mot_de_passe = ''.join(rchoice)
-        ligne.append(mot_de_passe)
-        User.append(ligne)
+        new_line.append(ligne[0])
+        new_line.append(ligne[1])
+        new_line.append(ligne[3])
+        new_line.append(prenom_nom_propre)
+        new_line.append(mot_de_passe)
+        User.append(new_line)
 
 new = "output/account.csv"
 with open(new, 'w', newline='', encoding='utf-8') as fichier_csv:
